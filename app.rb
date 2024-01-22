ebf668d54856e11549048088ec216ae0d2bababb
Douglas Berkley <douglasmberkley@gmail.com>
2024-01-22 18:10:11 +0900

    changed csv variable names

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ require_relative 'app/controllers/meals_controller'
 require_relative 'app/controllers/customers_controller'
 require_relative 'router'
 
-csv_file_path = 'data/meals.csv'
-meal_repository = MealRepository.new(csv_file_path)
+meals_csv_file_path = 'data/meals.csv'
+meal_repository = MealRepository.new(meals_csv_file_path)
 meals_controller = MealsController.new(meal_repository)
 
-csv_file_path = 'data/customers.csv'
-customer_repository = CustomerRepository.new(csv_file_path)
+customers_csv_file_path = 'data/customers.csv'
+customer_repository = CustomerRepository.new(customers_csv_file_path)
 customers_controller = CustomersController.new(customer_repository)
 
 router = Router.new(meals_controller, customers_controller)
